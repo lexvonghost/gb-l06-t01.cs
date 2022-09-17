8a419788631ae0e0027e11555e29919d5a72da84
lexvonghost <lex_von_tost@mail.ru>
2022-09-17 17:41:04 +0300

    feat: решение

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+Console.WriteLine("Введите число M");
+int m = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+Console.WriteLine("Введите М чисел");
+for (int i = 0; i < m; i++)
+{
+   int n = Convert.ToInt32(Console.ReadLine());
+   if (n > 0)
+   {
+        count++;
+   }
+}
+Console.WriteLine("Ответ");
+Console.WriteLine(count);
